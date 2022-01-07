5432ac651c5dc257f1d8dff6e11588c90313a19a
fangliangs <fangliangs@graphcore.ai>
2022-01-07 11:02:12 +0800

    update pretrain

--- pretrain.sh
+++ pretrain.sh
@@ -8,5 +8,5 @@ OUTPUT_DIR=/pretrain
 # 1 replica
 nohup python -u src/pretrain/run_pretrain.py \
     --config src/configs/pretrain_image_text_base_resnet50_mlm_itm.json \
-    &> pretrain_m2000_bs1.log &
+    &> pretrain_m2000_bs2.log &
 
